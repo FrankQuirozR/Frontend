f126e7eef226ea9124414006b75a4295719d27f3
frank.quiroz.ramirez.082000@gmail.com <165614620+FrankQuirozR@users.noreply.github.com>
2024-11-14 11:59:49 -0500

    first commit

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  // app.use(
+  //   '/dwc',
+  //   createProxyMiddleware({
+  //     target: 'http://localhost:3003/webair',
+  //     changeOrigin: true,
+  //   })
+  // );
+};
